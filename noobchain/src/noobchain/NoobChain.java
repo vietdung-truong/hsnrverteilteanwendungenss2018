ba41cd5e68739b9f0828f041f8fee788586efe5e
Viet-Dung Truong <deivatko@gmail.com>
2018-04-22 14:27:29 +0200

    test Commit 2

--- noobchain/src/noobchain/NoobChain.java
+++ noobchain/src/noobchain/NoobChain.java
@@ -18,4 +18,25 @@ public class NoobChain {
 		System.out.println(blockchainJson);
 	
 	}
+	
+	public static Boolean isChainValid() {
+		Block currentBlock;
+		Block previousBlock;
+		
+		//loop through blockchain to check hashes:
+			for (int i = 1; i < blockchain.size(); i++) {
+				currentBlock = blockchain.get(i);
+				previousBlock = blockchain.get(i-1);
+			if(!currentBlock.hash.equals(currentBlock.calculateHash())) {
+				System.out.println("Current Hashes are not equal!");
+				return false;
+			}
+			if(!previousBlock.hash.equals(currentBlock.previoushash)) {
+				System.out.println("previous Hashes not equal");
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }

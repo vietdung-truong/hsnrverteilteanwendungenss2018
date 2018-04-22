ccecdbe13d5d0ff14b8b9644a5ed321a305bbf41
Viet-Dung Truong <deivatko@gmail.com>
2018-04-22 23:54:40 +0200

    Last commit. Finished stage 1.

--- noobchain/src/noobchain/Block.java
+++ noobchain/src/noobchain/Block.java
@@ -7,10 +7,16 @@ import java.util.Date;
 
 public class Block {
 	
+	/* @Author David
+	 * this is a copy of the implementation from the website
+	 * https://medium.com/programmers-blockchain/create-simple-blockchain-java-tutorial-from-scratch-6eeed3cb03fa
+	 */
+	
 	public String hash;
 	public String previoushash;
 	private String data;
-	private long timeStamp;
+	private long timeStamp; //time of creation of the block
+	private int nonce;
 	
 	//Constructor
 	public Block(String data,String previoushash) {
@@ -24,8 +30,18 @@ public class Block {
 		String calculatedhash = StringUtil.applySha256(
 				previoushash +
 				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
 				data
 				);
 		return calculatedhash;
+	}
+	
+	public void mineBlock(int dificulty) {
+		String target = new String(new char[dificulty]).replace('\0', '0'); //)
+		while(!hash.substring(0,dificulty).equals(target)) {
+			nonce++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!!: " + hash);
 	}
 }
--- noobchain/src/noobchain/NoobChain.java
+++ noobchain/src/noobchain/NoobChain.java
@@ -6,22 +6,33 @@ import com.google.gson.GsonBuilder;
 public class NoobChain {
 	
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int dificulty = 5;
 
 	public static void main(String[] args) {
 		
 		
 		blockchain.add(new Block ("Hi im the first block", "0"));
+		System.out.println("Trying to mine...");
+		blockchain.get(0).mineBlock(dificulty);
 		blockchain.add(new Block ("Yo im the second block and I have extra text for you",blockchain.get(blockchain.size()-1).hash));
+		System.out.println("Trying to mine...");
+		blockchain.get(1).mineBlock(dificulty);
 		blockchain.add(new Block ("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+		System.out.println("Trying to mine...");
+		blockchain.get(2).mineBlock(dificulty);
 		
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+		
+		System.out.println('\0');
 		System.out.println(blockchainJson);
+		
 	
 	}
 	
 	public static Boolean isChainValid() {
 		Block currentBlock;
 		Block previousBlock;
+		String hashTarget = new String(new char[dificulty]).replace('\0', '0');
 		
 		//loop through blockchain to check hashes:
 			for (int i = 1; i < blockchain.size(); i++) {
@@ -33,6 +44,10 @@ public class NoobChain {
 			}
 			if(!previousBlock.hash.equals(currentBlock.previoushash)) {
 				System.out.println("previous Hashes not equal");
+				return false;
+			}
+			if(!currentBlock.hash.substring(0,dificulty).equals(hashTarget)) {
+				System.out.println("This hasn't been mined yet");
 				return false;
 			}
 		}

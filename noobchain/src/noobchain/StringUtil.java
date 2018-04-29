1ad06d905be06f49feb0baeadbe12cb9838f890d
Viet-Dung Truong <deivatko@gmail.com>
2018-04-29 18:05:14 +0200

    commit transaction update and adding ECDSA to StringUtil

--- noobchain/src/noobchain/StringUtil.java
+++ noobchain/src/noobchain/StringUtil.java
@@ -1,6 +1,7 @@
 package noobchain;
 
-import java.security.MessageDigest;
+import java.security.*;
+import java.util.Base64;
 
 public class StringUtil {
 	//applies Sha256 to a string and returns the result.
@@ -19,5 +20,39 @@ public class StringUtil {
 		catch(Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public static byte[] applyECDSASig(PrivateKey privateKey, String input) { 
+		//Created after Transaction. Not sure for what use
+		Signature dsa;
+		byte[] output = new byte[0];
+		try {
+			dsa = Signature.getInstance("ECDSA", "BC");
+			dsa.initSign(privateKey);
+			byte[] strByte = input.getBytes();
+			dsa.update(strByte);
+			byte[] realSig = dsa.sign();
+			output = realSig;
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return output;
+	}
+	
+	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+		try {
+			Signature ecdsaVerify = Signature.getInstance("ECSSA", "BC");
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String getStringFromKey(Key key) {
+		// TODO Auto-generated method stub
+		//Created after Editing Transaction
+		return Base64.getEncoder().encodeToString(key.getEncoded());
 	}
 }
--- noobchain/src/noobchain/Transaction.java
+++ noobchain/src/noobchain/Transaction.java
@@ -11,4 +11,23 @@ public class Transaction {
 	
 	public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
 	public ArrayList<TransactionOutput> outputs =  new ArrayList<TransactionOutput>();
+	
+	private static int sequence = 0;
+	
+	//Konstruktor
+	public Transaction(PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs) {
+		this.sender = from;
+		this.recipient = to;
+		this.value = value;
+		this.inputs = inputs;
+	}
+	
+	private String calculateHash() {
+		sequence ++;
+		return StringUtil.applySha256(
+				StringUtil.getStringFromKey(sender) +
+				StringUtil.getStringFromKey(recipient) +
+				Float.toString(value) + sequence);
+		
+	}
 }

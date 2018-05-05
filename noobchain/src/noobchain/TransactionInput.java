ff40edee91f1725894d9878bd2f2180c355e9663
deiva <deiva@LAPTOP-N31IP1LC>
2018-05-05 18:20:12 +0200

    Commit transaction Input and Output done

--- noobchain/src/noobchain/TransactionInput.java
+++ noobchain/src/noobchain/TransactionInput.java
@@ -1,5 +1,13 @@
 package noobchain;
 
 public class TransactionInput {
+	public String transactionOutputId;
+	public TransactionOutput UTXO; //unspent transaction output;
 
+	public TransactionInput (String transationOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
 }
+
+//this class contains all of the input targeted to you, that
+//means how much coins you have

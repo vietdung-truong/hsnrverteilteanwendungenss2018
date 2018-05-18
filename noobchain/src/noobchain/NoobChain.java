22fbe61862376d3cc1921e68aff1954bcd934203
Viet-Dung Truong <deivatko@gmail.com>
2018-05-18 23:44:42 +0200

    Final PC working

--- noobchain/src/noobchain/NoobChain.java
+++ noobchain/src/noobchain/NoobChain.java
@@ -12,7 +12,7 @@ public class NoobChain {
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
 	public static HashMap<String,TransactionOutput> UTXOs = new HashMap <String,TransactionOutput>();
 	
-	public static int dificulty = 4; //Commit Test Mo
+	public static int dificulty = 5; //Commit Test Mo
 	public static float minimumTransaction = 0.1f;
 	public static Wallet walletA;
 	public static Wallet walletB;
